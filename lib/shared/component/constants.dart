eaa125680e5d58311518cf54acc1f1e747b9e011
islamgharib <islamahmed1992@yahoo.com>
2022-01-05 21:30:03 +0200

    initial bloc packages

---
+++ lib/shared/component/constants.dart
@@ -0,0 +1,17 @@
+
+                    /* article api
+api :- https://newsapi.org/v2/top-headlines?country=eg&category=business&apiKey=f3c27252eb1645b3bd787ff3d9b1f17e
+
+baseurl :- https://newsapi.org/
+method :- v2/top-headlines?
+queries :- country=eg&category=business&apiKey=f3c27252eb1645b3bd787ff3d9b1f17e
+category -> business - science - sports
+*/
+
+                    /* search api
+api :- https://newsapi.org/v2/everything?q=s&apiKey=f3c27252eb1645b3bd787ff3d9b1f17e
+
+baseurl :- https://newsapi.org/
+method :- v2/everything?
+queries :- q=s&apiKey=f3c27252eb1645b3bd787ff3d9b1f17e
+*/
