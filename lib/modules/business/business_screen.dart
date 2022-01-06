2207dae25261ab1bb3ce835361f6b96bb6a53fbe
islamgharib <islamahmed1992@yahoo.com>
2022-01-06 15:50:05 +0200

    create article item widget

--- lib/modules/business/business_screen.dart
+++ lib/modules/business/business_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/layout/cubit/news_cubit.dart';
 import 'package:news_app/layout/cubit/news_states.dart';
+import 'package:news_app/shared/component/components.dart';
 
 class Business extends StatelessWidget {
 
--- lib/shared/component/components.dart
+++ lib/shared/component/components.dart
@@ -21,3 +21,63 @@ void navigateTo(context, widget) => Navigator.push(
     builder: (context) => widget,
   ),
 );
+
+Widget buildArticleItem(article, context) =>
+    Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Container(
+            height: 120.0,
+            width: 120.0,
+
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: NetworkImage('${article['urlToImage']}'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(width: 20.0,),
+          Expanded(
+            child: Container(
+              height: 120.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                //mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      '${article['title']}',
+                      style: Theme.of(context).textTheme.bodyText1,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Text(
+                    '${article['publishedAt']}',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+
+Widget myDivider() =>
+    Padding(
+      padding: const EdgeInsetsDirectional.only(
+        start: 20.0,
+      ),
+      child: Container(
+        width: double.infinity,
+        height: 1.0,
+        color: Colors.grey[300],
+      ),
+    );
