6639802954dcd284e5debcd7b80cc3977b0cbfa0
islamgharib <islamahmed1992@yahoo.com>
2022-01-06 15:42:34 +0200

    creating business screen

--- lib/modules/business/business_screen.dart
+++ lib/modules/business/business_screen.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/layout/cubit/news_cubit.dart';
+import 'package:news_app/layout/cubit/news_states.dart';
 
 class Business extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Business',
-      ),
+    return BlocConsumer<NewsCubit, NewsStates>(
+      listener: (context, state){},
+      builder: (context, state){
+        var list = NewsCubit.get(context).business;
+        return articleBuilder(list);
+      },
     );
   }
 }
