0d1be8071ef24a197e90345e072eea8955981563
islamgharib <islamahmed1992@yahoo.com>
2022-01-07 11:48:22 +0200

    handling brightness mode inside main

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,49 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/shared/bloc_provider.dart';
+import 'package:news_app/shared/cubit/app_cubit.dart';
+import 'package:news_app/shared/cubit/app_states.dart';
+import 'package:news_app/shared/network/local/cach_helper.dart';
 import 'package:news_app/shared/network/remote/dio_helper.dart';
+import 'package:news_app/shared/styles/themes.dart';
 
 import 'layout/news_layout.dart';
 
-void main() {
+void main() async{
+  // to ensure every await statements is implemented before runApp
+  WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   DioHelper.init();
-  
-
-  runApp(MyApp());
+  await CachHelper.init();
+  bool? isDark = CachHelper.getBoolean(key: 'isDark');
+  runApp(MyApp(isDark!));
 }
 
 class MyApp extends StatelessWidget {
+  final bool isDark;
+  MyApp(this.isDark);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: NewsLayout(),
+    return BlocProvider(
+      create:(BuildContext context) => AppCubit()..changeAppMode(fromShared: isDark),
+      child: BlocConsumer<AppCubit, AppStates>(
+        listener: (context, state){},
+        builder: (context, state){
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            // light mode
+            theme: lightTheme,
+            // dark mode
+            darkTheme: darkTheme,
+            themeMode: AppCubit.get(context).isDark ? ThemeMode.dark : ThemeMode.light,
+            home: NewsLayout(),
+          );
+        },
+      ),
     );
+
+
   }
 }
-
